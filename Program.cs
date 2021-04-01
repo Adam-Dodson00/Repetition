4032021cf1660fa20335a91f43f57fd4765fa60e
ADAM DODSON <103440049@student.swin.edu.au>
2021-04-01 15:27:53 +1100

    nested rep

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@ namespace Repetition{
     class Program{
         static void Main(string[] args){
             
-            
+            Console.WriteLine("What is your name");
+            string name = Console.ReadLine();
+
+            int counter = 0;
+
+            while( counter < 10){
+                int counter2 = counter;
+
+                while (counter2 < 5){
+                    Console.WriteLine(name);
+                    counter2++;
+                    Console.WriteLine(counter2);
+                }
+                counter++;
+            }
+            Console.WriteLine("Goodbye " + name);
             
         }
     }
