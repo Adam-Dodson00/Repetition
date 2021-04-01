d5659d4e9ea42f207b804116aa5282c03633d8b8
ADAM DODSON <103440049@student.swin.edu.au>
2021-04-01 16:16:04 +1100

    after code review

--- Program.cs
+++ Program.cs
@@ -2,20 +2,21 @@
 
 namespace Repetition{
     class Program{
-        static void Main(string[] args){
+        static void Main(string[] args) {
             
             Console.WriteLine("What is your name");
             string name = Console.ReadLine();
 
             int counter = 0;
 
-            while(counter < 10){
+            while(counter < 10) {
                 int counter2 = counter;
-
-                while (counter2 < 5){
+                
+                while (counter2 < 5) {
                     Console.Write(name);
                     counter2++;
                 }
+
                 Console.WriteLine();
                 counter++;
             }
