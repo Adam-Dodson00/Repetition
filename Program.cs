106dc694259d9fe503b899ab447b728ad7b2bf6d
ADAM DODSON <103440049@student.swin.edu.au>
2021-04-01 14:58:53 +1100

    start fresh

--- Program.cs
+++ Program.cs
@@ -4,24 +4,8 @@ namespace Repetition{
     class Program{
         static void Main(string[] args){
             
-
-            Console.WriteLine("How many times would you like this to repeat?");
-            //int repeat = int.Parse(Console.ReadLine());
-
-            string num = Console.ReadLine();
-            int repeat = int.Parse(num);
-
-            int count = 0;
             
-            Console.WriteLine("Printing your name " + repeat + " times");
-
-            while(repeat > count){
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-                Console.WriteLine(name);
-                count++;
-            }
-            Console.WriteLine("Goodbye ");
+            
         }
     }
 }
